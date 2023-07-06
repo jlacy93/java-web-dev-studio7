c50620d972836a92046c8a3af66452275a26497e
jlacy <josephinalacy@gmail.com>
2023-07-05 20:31:42 -0500

    added DVD

--- src/org/launchcode/studio7/DVD.java
+++ src/org/launchcode/studio7/DVD.java
@@ -1,6 +1,29 @@
 package org.launchcode.studio7;
 
-public class DVD {
+public class DVD extends BaseDisc implements OpticalDisc {
+    public DVD(String name, int totalCapacity) {
+        super(name, totalCapacity);
+    }
+
+    @Override
+    public void spinDisc() {
+
+    }
+
+    @Override
+    public void ejectDisc() {
+
+    }
+
+    @Override
+    public void getMetadata() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
 
     // TODO: Implement your custom interface.
 
